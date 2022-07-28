5d2c8d36d09fc91aff2d5345fc4f26716149c5eb
abukhalil2b <abukhalil2b>
2022-07-28 09:01:36 +0400

    f

---
+++ student/js/app.js
@@ -0,0 +1,20 @@
+let memoSowars = document.querySelectorAll(".memo_sowars > div");
+let memoJuzs = document.querySelectorAll(".memo_juzs > div");
+
+for (let index = 0; index < memoSowars.length; index++) {
+    const element = memoSowars[index];
+    console.log(element);
+  
+    element.addEventListener("click", () => {
+      element.classList.toggle("selected");
+    });
+  }
+  
+for (let index = 0; index < memoJuzs.length; index++) {
+  const element = memoJuzs[index];
+  console.log(element);
+
+  element.addEventListener("click", () => {
+    element.classList.toggle("selected");
+  });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html","./sadakat/*.html"],
+  theme: {
+    extend: {
+      colors:{
+      primary:'#ffc83b'
+    }
+    },
+    
+  },
+  plugins: [],
+}
