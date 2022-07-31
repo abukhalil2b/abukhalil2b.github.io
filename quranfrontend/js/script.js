44071c5daf49cfb938ec017ef0ea564e9e883fe7
abukhalil2b <abukhalil2b>
2022-07-31 23:51:51 +0400

    f

--- quranfrontend/js/script.js
+++ quranfrontend/js/script.js
@@ -1,8 +1,25 @@
-const clip1 = document.getElementById('clip1');
+const rightClip = document.getElementById("rightClip");
 
-	clip1.addEventListener('mouseenter',()=>{
-		clip1.play()
-	})
-	clip1.addEventListener('mouseout',()=>{
-		clip1.pause()
-	})
+const rightClipControl = document.getElementById("rightClipControl");
+
+rightClipControl.addEventListener("mouseenter", () => {
+  rightClip.play();
+});
+
+rightClipControl.addEventListener("mouseout", () => {
+  rightClip.pause();
+});
+
+// left
+
+const leftClip = document.getElementById("leftClip");
+
+const leftClipControl = document.getElementById("leftClipControl");
+
+leftClipControl.addEventListener("mouseenter", () => {
+  leftClip.play();
+});
+
+leftClipControl.addEventListener("mouseout", () => {
+  leftClip.pause();
+});
--- student/js/app.js
+++ student/js/app.js
@@ -2,18 +2,14 @@ let memoSowars = document.querySelectorAll(".memo_sowars > div");
 let memoJuzs = document.querySelectorAll(".memo_juzs > div");
 
 for (let index = 0; index < memoSowars.length; index++) {
-    const element = memoSowars[index];
-    console.log(element);
-  
-    element.addEventListener("click", () => {
-      element.classList.toggle("selected");
-    });
-  }
-  
+  const element = memoSowars[index];
+  element.addEventListener("click", () => {
+    element.classList.toggle("selected");
+  });
+}
+
 for (let index = 0; index < memoJuzs.length; index++) {
   const element = memoJuzs[index];
-  console.log(element);
-
   element.addEventListener("click", () => {
     element.classList.toggle("selected");
   });
